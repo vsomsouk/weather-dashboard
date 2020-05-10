5448a4042d734181b6c7287911c8ee0c6ac23710
Vanida Somchaleunsouk <vsomsouk@gmail.com>
2020-05-09 20:45:23 -0500

    added api key to js file

---
+++ Assets/JS/script.js
@@ -0,0 +1,20 @@
+//api key
+//add value of input
+
+
+
+
+
+    let city= document.getElementById ("cityName").value;
+
+
+const apikey = "&appid=beeea19259945355a46d5432f515ce32";
+
+
+
+}
+
+
+//value from user/search
+//url to call api link
+//info to page
