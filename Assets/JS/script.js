efe6aa73f9d6d0e82e52082e6869158aaba6923c
Vanida Somchaleunsouk <vsomsouk@gmail.com>
2020-05-09 22:49:05 -0500

    re-did html and js file.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,18 +1,26 @@
-//api key
-//add value of input
 
 
 
-function onload() {
+//button
+$("searchBtn").on("click", function(event) {
+    event.preventDefault();
 
+    var apiKey = "&appid=beeea19259945355a46d5432f515ce32";
 
+    var getWeather = $("#get-weather");
+    var city = getWeather.val().trim();
+    cities.push(city)
+    var message = document.querySelector(".invalid-message");
+     
+    console.log(getWeather);
 
+    var queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?lat=latitude&lon=longitude&q=" +
+    city + "&appid=" + apiKey;
 
-const apikey = "&appid=beeea19259945355a46d5432f515ce32";
-
-//add event listener
-
-
-
-//value from user/search
-//url to call api link
+    //GET AJAX
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+ 
