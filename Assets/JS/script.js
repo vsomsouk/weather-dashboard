3eaf3844482666e694405fc3c84c038549d77e7d
Vanida Somchaleunsouk <vsomsouk@gmail.com>
2020-05-19 10:13:25 -0500

    Got the API to connect

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,13 +1,26 @@
-var url = "https://api.openweathermap.org/data/2.5/forecast?q=";
-var currenturl = "https://api.openweathermap.org/data/2.5/weather?q=";
+//var url = "https://api.openweathermap.org/data/2.5/forecast?q=";
+//var currenturl = "https://api.openweathermap.org/data/2.5/weather?q=";
 
+
+//search button
 var btn = document.querySelector("#button");
-var city = document.querySelector("#cityName");
+//text box where you input the city name
+var cityName = document.querySelector("#cityName");
 
-var apiKey = "&appid=beeea19259945355a46d5432f515ce32";
+var apiKey = "b0687da62cb3bc7b420e13260ea2df0b";
 
-    // add event listener to button
-    btn.addEventListener("click", function() {
-       // alert("hello");
-    })
-  
+
+
+btn.onclick = function(){
+  const city = cityName.value;
+  var apiURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
+  console.log(apiURL);
+  fetch(apiURL).then(response => {
+  response.json().then(json => {
+    let data = json;
+    console.log (data);
+  //  let output = formatResponse(data);
+  //  resultOut.innerHTML = output;
+  });
+ });
+}
