5589f8c10611196786d63b75a0475629428e3700
Vanida Somchaleunsouk <vsomsouk@gmail.com>
2020-05-19 23:27:26 -0500

    Pulled current location data from api to web page

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -7,6 +7,13 @@ var btn = document.querySelector("#button");
 //text box where you input the city name
 var cityName = document.querySelector("#cityName");
 
+var result = document.querySelector("currentCity")
+var locationdate = document.querySelector("locationdate");
+var temp = document.querySelector("temp");
+var humidity = document.querySelector("humidity");
+var wind = document.querySelector("wind");
+var UV = document.querySelector("UV")
+
 var apiKey = "b0687da62cb3bc7b420e13260ea2df0b";
 
 
@@ -15,12 +22,26 @@ btn.onclick = function(){
   const city = cityName.value;
   var apiURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
   console.log(apiURL);
-  fetch(apiURL).then(response => {
-  response.json().then(json => {
-    let data = json;
-    console.log (data);
-  //  let output = formatResponse(data);
-  //  resultOut.innerHTML = output;
+  $.get(apiURL, function(data) {
+    console.log(data);
+    $("#locationname").text(data.name);
+    $("#tempText").text(data.main.temp);
+    $("#humidityText").text(data.main.humidity);
+    $("#windText").text(data.wind.speed);
+    //$("#uvText").text(data.main.);
+
+    let otherApi =  'https://api.openweathermap.org/data/2.5/onecall?lat=' + data.main.lat + '&lon=' + data.main.lon + '&appid=' + apiKey;
+    $.get(otherApi, function(forecast) {
+      // add forecast to HTML
+    })
   });
- });
+//   fetch(apiURL).then(response => {
+//   response.json().then(json => {
+//     let data = json;
+//     console.log (data);
+//     $("#tempText").text(data.main.temp);
+//   });
+//  });
 }
+
+
