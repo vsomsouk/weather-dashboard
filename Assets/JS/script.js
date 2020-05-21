a9761658401b2a87fab59de9ab7f91424f8b24f1
Vanida Somchaleunsouk <vsomsouk@gmail.com>
2020-05-21 18:55:19 -0500

    cleaned up js file again

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -3,10 +3,9 @@ var btn = document.querySelector("#button");
 
 //text box where you input the city name
 var cityName = document.querySelector("#cityName");
-
 var apiKey = "b0687da62cb3bc7b420e13260ea2df0b";
 
-
+//when I click the search button...
 btn.onclick = function(){
   const city = cityName.value;
   var apiURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial' + '&appid=' + apiKey;
@@ -19,7 +18,6 @@ btn.onclick = function(){
     $("#humidityText").text(data.main.humidity);
     $("#windText").text(data.wind.speed);
 
-
     //adding a date to current day and 5 day forecast.
     let currentDate = moment().calendar("DD-MM-YYYY");
     let currentAnd1 = moment().add(1, 'days').calendar("DD-MM-YYYY");
@@ -28,7 +26,6 @@ btn.onclick = function(){
     let currentAnd4 = moment().add(4, 'days').calendar("DD-MM-YYYY");
     let currentAnd5 = moment().add(5, 'days').calendar("DD-MM-YYYY");
     console.log(currentDate);
-
 
     function displayDate () {
     $("#currentDate").text(currentDate);
@@ -40,60 +37,51 @@ btn.onclick = function(){
     };
     displayDate();
 
-
-    // one call API for 5 day and grabbing the UV for current 
+    // one call API for 5 day forecast
     let otherApi =  'https://api.openweathermap.org/data/2.5/onecall?lat=' + data.coord.lat + '&lon=' + data.coord.lon + '&units=imperial' +'&appid=' + apiKey;
     console.log(otherApi);
     $.get(otherApi, function(forecast) {
     console.log(forecast);
 
-
     //Pull the UV from one call API to current weather location div
     $("#uvText").text(forecast.current.uvi);
+
+    //Temp, Humidty & Icon for 5 Day Forecast
 
     //5-day forecast Day 1
     $("#dayOne").text(forecast.daily[1].temp.day);
     $("#humid1").text(forecast.daily[1].humidity);
-
-    //5-day forecast Day 2
-    $("#dayTwo").text(forecast.daily[2].temp.day);
-    $("#humid2").text(forecast.daily[2].humidity);
-
-    //5-day forecast Day 3
-    $("#dayThree").text(forecast.daily[3].temp.day);
-    $("#humid3").text(forecast.daily[3].humidity);
-
-    //5-day forecast Day 4
-    $("#dayFour").text(forecast.daily[4].temp.day);
-    $("#humid4").text(forecast.daily[4].humidity);
-
-    //5-day forecast Day 5
-    $("#dayFive").text(forecast.daily[5].temp.day);
-    $("#humid5").text(forecast.daily[5].humidity);
-
-
-    // adding icon to 5 day foreast
-   
     var icon1 = forecast.daily[1].weather[0].icon;
     var iconURL1 = "http://openweathermap.org/img/w/" + icon1 + ".png";
     $(".icon1").attr('src', iconURL1);
 
+    //5-day forecast Day 2
+    $("#dayTwo").text(forecast.daily[2].temp.day);
+    $("#humid2").text(forecast.daily[2].humidity);
     var icon2 = forecast.daily[2].weather[0].icon;
     var iconURL2 = "http://openweathermap.org/img/w/" + icon2 + ".png";
     $(".icon2").attr('src', iconURL1);
 
+    //5-day forecast Day 3
+    $("#dayThree").text(forecast.daily[3].temp.day);
+    $("#humid3").text(forecast.daily[3].humidity);
     var icon3 = forecast.daily[3].weather[0].icon;
     var iconURL3 = "http://openweathermap.org/img/w/" + icon3 + ".png";
     $(".icon3").attr('src', iconURL1);
 
+    //5-day forecast Day 4
+    $("#dayFour").text(forecast.daily[4].temp.day);
+    $("#humid4").text(forecast.daily[4].humidity);
     var icon4 = forecast.daily[4].weather[0].icon;
     var iconURL4 = "http://openweathermap.org/img/w/" + icon4 + ".png";
     $(".icon4").attr('src', iconURL1);
 
+    //5-day forecast Day 5
+    $("#dayFive").text(forecast.daily[5].temp.day);
+    $("#humid5").text(forecast.daily[5].humidity);
     var icon5 = forecast.daily[5].weather[0].icon;
     var iconURL5 = "http://openweathermap.org/img/w/" + icon5 + ".png";
     $(".icon5").attr('src', iconURL1);
-
  
     //creating a list of cities
     var cityList ='<div class="row"></div>';
