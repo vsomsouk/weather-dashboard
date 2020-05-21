593c5916855d91c618c7f35a26f3a25241c40944
Vanida Somchaleunsouk <vsomsouk@gmail.com>
2020-05-20 22:48:32 -0500

    added one call to grab the 5 day forecast and switched metric to imperial for temp. Change and adjusted formatting on html on how the data displays on the page. Used moment for displaying the date.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -18,21 +18,66 @@ var apiKey = "b0687da62cb3bc7b420e13260ea2df0b";
 
 
 
+
 btn.onclick = function(){
   const city = cityName.value;
-  var apiURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
+  var apiURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial' + '&appid=' + apiKey;
   console.log(apiURL);
   $.get(apiURL, function(data) {
     console.log(data);
+    
     $("#locationname").text(data.name);
     $("#tempText").text(data.main.temp);
     $("#humidityText").text(data.main.humidity);
     $("#windText").text(data.wind.speed);
-    //$("#uvText").text(data.main.);
+    //$("#currPhoto").append(data.weather[0].icon);
 
-    let otherApi =  'https://api.openweathermap.org/data/2.5/onecall?lat=' + data.main.lat + '&lon=' + data.main.lon + '&appid=' + apiKey;
+    //adding a date to current day and 5 day forecast
+    let currentTime = moment().subtract(10, 'days').calendar();
+    console.log(currentTime);
+
+    function displayDate () {
+    $("#currentDate").text(currentTime);
+    };
+    displayDate();
+    
+    // one call API for 5 day and grabbing the UV for current 
+    let otherApi =  'https://api.openweathermap.org/data/2.5/onecall?lat=' + data.coord.lat + '&lon=' + data.coord.lon + '&units=imperial' +'&appid=' + apiKey;
+    console.log(otherApi);
     $.get(otherApi, function(forecast) {
-      // add forecast to HTML
+    console.log(forecast);
+
+    //Pull the UV from one call API to current weather location div
+    $("#uvText").text(forecast.current.uvi);
+
+    //5-day forecast Day 1
+    $("#dayOne").text(forecast.daily[1].temp.day);
+    $("#humid1").text(forecast.daily[1].humidity);
+
+    //5-day forecast Day 2
+    $("#dayTwo").text(forecast.daily[2].temp.day);
+    $("#humid2").text(forecast.daily[2].humidity);
+
+    //5-day forecast Day 3
+    $("#dayThree").text(forecast.daily[3].temp.day);
+    $("#humid3").text(forecast.daily[3].humidity);
+
+    //5-day forecast Day 4
+    $("#dayFour").text(forecast.daily[4].temp.day);
+    $("#humid4").text(forecast.daily[4].humidity);
+
+    //5-day forecast Day 5
+    $("#dayFive").text(forecast.daily[5].temp.day);
+    $("#humid5").text(forecast.daily[5].humidity);
+ 
+
+    //save in local storage
+
+
+    //localStorage.setItem("cities", JSON.stringify(data));
+
+    
+    
     })
   });
 //   fetch(apiURL).then(response => {
