cb7fe1fb0791c797ed738ba1a6889a335c46cba5
Vanida Somchaleunsouk <vsomsouk@gmail.com>
2020-05-21 22:31:17 -0500

    Fixed cities div list when adding a new city to create a list. Added local storage.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -20,20 +20,20 @@ btn.onclick = function(){
 
     //adding a date to current day and 5 day forecast.
     let currentDate = moment().calendar("DD-MM-YYYY");
-    let currentAnd1 = moment().add(1, 'days').calendar("DD-MM-YYYY");
-    let currentAnd2 = moment().add(2, 'days').calendar("DD-MM-YYYY");
-    let currentAnd3 = moment().add(3, 'days').calendar("DD-MM-YYYY");
-    let currentAnd4 = moment().add(4, 'days').calendar("DD-MM-YYYY");
-    let currentAnd5 = moment().add(5, 'days').calendar("DD-MM-YYYY");
+    let currentAdd1 = moment().add(1, 'days').calendar("DD-MM-YYYY");
+    let currentAdd2 = moment().add(2, 'days').calendar("DD-MM-YYYY");
+    let currentAdd3 = moment().add(3, 'days').calendar("DD-MM-YYYY");
+    let currentAdd4 = moment().add(4, 'days').calendar("DD-MM-YYYY");
+    let currentAdd5 = moment().add(5, 'days').calendar("DD-MM-YYYY");
     console.log(currentDate);
 
     function displayDate () {
     $("#currentDate").text(currentDate);
-    $("#date1").text(currentAnd1);
-    $("#date2").text(currentAnd2);
-    $("#date3").text(currentAnd3);
-    $("#date4").text(currentAnd4);
-    $("#date5").text(currentAnd5);
+    $("#date1").text(currentAdd1);
+    $("#date2").text(currentAdd2);
+    $("#date3").text(currentAdd3);
+    $("#date4").text(currentAdd4);
+    $("#date5").text(currentAdd5);
     };
     displayDate();
 
@@ -43,10 +43,10 @@ btn.onclick = function(){
     $.get(otherApi, function(forecast) {
     console.log(forecast);
 
-    //Pull the UV from one call API to current weather location div
+    //pull the UV from one call API to current weather location div
     $("#uvText").text(forecast.current.uvi);
 
-    //Temp, Humidty & Icon for 5 Day Forecast
+    //temp, humidity & icon for 5 day forecast
 
     //5-day forecast Day 1
     $("#dayOne").text(forecast.daily[1].temp.day);
@@ -60,41 +60,37 @@ btn.onclick = function(){
     $("#humid2").text(forecast.daily[2].humidity);
     var icon2 = forecast.daily[2].weather[0].icon;
     var iconURL2 = "http://openweathermap.org/img/w/" + icon2 + ".png";
-    $(".icon2").attr('src', iconURL1);
+    $(".icon2").attr('src', iconURL2);
 
     //5-day forecast Day 3
     $("#dayThree").text(forecast.daily[3].temp.day);
     $("#humid3").text(forecast.daily[3].humidity);
     var icon3 = forecast.daily[3].weather[0].icon;
     var iconURL3 = "http://openweathermap.org/img/w/" + icon3 + ".png";
-    $(".icon3").attr('src', iconURL1);
+    $(".icon3").attr('src', iconURL3);
 
     //5-day forecast Day 4
     $("#dayFour").text(forecast.daily[4].temp.day);
     $("#humid4").text(forecast.daily[4].humidity);
     var icon4 = forecast.daily[4].weather[0].icon;
     var iconURL4 = "http://openweathermap.org/img/w/" + icon4 + ".png";
-    $(".icon4").attr('src', iconURL1);
+    $(".icon4").attr('src', iconURL4);
 
     //5-day forecast Day 5
     $("#dayFive").text(forecast.daily[5].temp.day);
     $("#humid5").text(forecast.daily[5].humidity);
     var icon5 = forecast.daily[5].weather[0].icon;
     var iconURL5 = "http://openweathermap.org/img/w/" + icon5 + ".png";
-    $(".icon5").attr('src', iconURL1);
+    $(".icon5").attr('src', iconURL5);
  
     //creating a list of cities
-    var cityList ='<div class="row"></div>';
-    $(".cities").append(cityList);
+    $(".cities").append("<ul class='c'>" + city + "</ul>");
 
-    // this keeps duplicating the the city name when a new row gets appended. 
-    //How do I prevent this?
-    $(".row").text(city);
 
     })
 
     //save in local storage
-    //localStorage.setItem("cities",JSON.stringify(data));
+    localStorage.setItem("city",JSON.stringify(data));
 
   });
 }
